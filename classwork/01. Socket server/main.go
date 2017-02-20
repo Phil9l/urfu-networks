9b11ea01b594543d547bcaf14599c840ea7cd0f0
phil9l <phil9lne@gmail.com>
2017-02-20 13:42:25 +0500

    Write echo server

---
+++ classwork/01. Socket server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "bufio"
+    "log"
+    "net"
+    "fmt"
+    "time"
+)
+
+const PORT = 8080
+const DELAY = 0 // In Millisecond
+
+func main() {
+    log.Printf("Starting echo-server on port %d", PORT)
+
+    ln, err := net.Listen("tcp", fmt.Sprintf(":%d", PORT))
+    if err != nil {
+        log.Fatalf("Listen error: %s", err)
+    }
+
+    for {
+            conn, err := ln.Accept()
+            if err != nil {
+                log.Fatalf("Accept error: %s", err)
+            }
+            go handleConnection(conn)
+            log.Println("New connection accepted.")
+    }
+}
+
+func handleConnection(conn net.Conn) {
+    buffer := bufio.NewReader(conn)
+
+    for {
+        line, err := buffer.ReadBytes('\n')
+        if err != nil {
+            break
+        }
+        log.Printf("Received new message: %s", line)
+        time.Sleep(DELAY * time.Millisecond)
+        conn.Write(line)
+    }
+}
